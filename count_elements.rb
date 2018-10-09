c810648db6cbcc2409b9512d07bad5624ec78159
Austin Luft <ausluft@gmail.com>
2018-10-09 19:17:39 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -5,9 +5,9 @@ def count_elements(array)
   binding.pry
   array.each do |element|
     if hash.has_key?(element) == false
-      hash[element][:count] = 1
+      hash[element]["count"] = 1
     else
-      hash[element][:count] += 1
+      hash[element]["count"] += 1
     end
   end
   return new_hash
