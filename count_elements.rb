e0b406b47221b5bc83ddde465ba43025a828ace5
Austin Luft <ausluft@gmail.com>
2018-10-09 19:28:51 +0000

    Done.

--- count_elements.rb
+++ count_elements.rb
@@ -1,4 +1,14 @@
+require 'pry'
+
 def count_elements(array)
-  # code goes here
+  new_hash = {}
+  array.each do |element|
+    if new_hash.has_key?(element) == false
+      # binding.pry
+      new_hash[element] = 1
+    else
+      new_hash[element] += 1
+    end
+  end
+  return new_hash
 end
- 
