a788ef893c1d126d9ec7a3013d85a2e61a97cecd
Austin Luft <ausluft@gmail.com>
2018-10-09 19:19:36 +0000

    automatically backed up by learn

--- count_elements.rb
+++ count_elements.rb
@@ -3,7 +3,7 @@ require 'pry'
 def count_elements(array)
   new_hash = {}
   array.each do |element|
-    if hash.has_key?(element) == false
+    if new_hash.has_key?(element) == false
       hash[element]["count"] = 1
     else
       hash[element]["count"] += 1
